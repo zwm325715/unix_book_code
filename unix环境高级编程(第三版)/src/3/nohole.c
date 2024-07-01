7810bb611c041ad3b0ad07d449852b6679d65ffe
zwm325715 <376110985.com>
2024-07-02 03:13:35 +0800

    2024-07-02 03:13:35

---
+++ unix环境高级编程(第三版)/src/3/nohole.c
@@ -0,0 +1,19 @@
+#include "apue.h"
+#include <fcntl.h>
+
+char	buf[] = "a";
+
+//图3-2:创建一个具有空洞的文件
+int main(void) {
+	int	fd;
+    int count;
+
+	if ((fd = creat("file.nohole", FILE_MODE)) < 0)
+		err_sys("creat nohole error");
+    for (count = 0; count < 16394; count++) {
+        if (write(fd, buf, 1) != 1)
+		    err_sys("buf1 write error");
+    }
+
+	exit(0);
+}
