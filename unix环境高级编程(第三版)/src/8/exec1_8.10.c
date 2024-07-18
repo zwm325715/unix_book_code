2e1445a36fdf84f356f917df1a6059e4dcac80f1
zwm325715 <376110985.com>
2024-07-18 17:21:11 +0800

    2024-07-18 17:21:11

---
+++ unix环境高级编程(第三版)/src/8/exec1_8.10.c
@@ -0,0 +1,39 @@
+#include "apue.h"
+#include <sys/wait.h>
+//字符串数组
+char *env_init[] = { "USER=unknown", "PATH=/tmp", NULL };
+// 拼接字符串
+#define PROJECT_DIR_FILE_STR(x,y) x#y
+// 图8-16中的程序演示了exec函数
+int main(void) {
+	pid_t	pid;
+	//创建子进程
+	if ((pid = fork()) < 0) {
+		err_sys("fork error");
+	} else if (pid == 0) {	//子进程执行
+		/*
+		1.specify pathname, specify environment  
+			指定pathname,指定环境变量
+		2.通过"参数列表"和"新环境变量"的"路径名"方式执行新程序
+			新程序:/home/sar/bin/echoall
+			列表参数:"echoall", "myarg1","MY ARG2", (char *)0
+			环境字符串数组:env_init
+		*/
+		if (execle(PROJECT_DIR_FILE_STR(ROOT_DIR,"echoall_8.10"),
+				"echoall","myarg1","MY ARG2", (char *)0, 
+				env_init) < 0)
+			err_sys("execle error");
+	}
+
+	if (waitpid(pid, NULL, 0) < 0)
+		err_sys("wait error");
+
+	if ((pid = fork()) < 0) {
+		err_sys("fork error");
+	} else if (pid == 0) {	/* specify filename, inherit environment */
+		if (execlp("echoall","echoall", "only 1 arg", (char *)0) < 0)
+			err_sys("execlp error");
+	}
+
+	exit(0);
+}
