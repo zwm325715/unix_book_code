60d292c59579d1bbd1b5fb129e5e831b55c82d01
zwm325715 <376110985.com>
2025-01-10 20:33:25 +0800

    2025-01-10 20:33:25

--- unpv1/src/5/sigchldwait_5-7.c
+++ unpv1/src/5/sigchldwait_5-7.c
@@ -6,6 +6,11 @@ void sig_chld(int signo) {
 
   pid = wait(&stat);//收尸子进程
   //在信号处理函数中不适合调用printf这样的标准I/O函数:11.18节会讨论
-  printf("child %d terminated\n", pid);
+  printf("wait finish! Child PID= %d terminated\n", pid);
+  /*暂时没懂
+   * 编程约定:显示给出return语句，即使是void类型
+   *   这么做的好处是，当某个系统调用被我们编写的某个信号处理函数中断时,
+   *   我们就可以得知该系统调用具体是被哪个信号处理函数的哪个return语句中断的.
+   */
   return;
 }
